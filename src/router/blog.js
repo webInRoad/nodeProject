3822f6139f21ae1e59c7cc0892f26c76b5ed073c
webInRoad <1040233545@qq.com>
2021-08-31 00:38:08 +0800

    feat:调用接口时登录验证

--- src/router/blog.js
+++ src/router/blog.js
@@ -6,29 +6,51 @@ const {
 	deleteBlog
 } = require('../controller/blog')
 const { SuccessModal, ErrorModal } = require('../model/resModal')
+
+const loginCheck = (req) => {
+	if (!req.session.username) {
+		return Promise.resolve(new ErrorModal('未登录'))
+	}
+}
 const handleBlogRouter = (req, res) => {
 	const method = req.method
 	const { id } = req.query
 
 	if (method === 'GET' && req.path === '/api/blog/list') {
+		let loginCheckResult = loginCheck(req)
+		if (!loginCheckResult) {
+			return loginCheckResult
+		}
 		const { author, keyword } = req.query
 		return getList(author, keyword).then((list) => {
 			return new SuccessModal(list)
 		})
 	}
 	if (method === 'GET' && req.path === '/api/blog/detail') {
+		let loginCheckResult = loginCheck(req)
+		if (!loginCheckResult) {
+			return loginCheckResult
+		}
 		return getDetail(id).then((list) => {
 			return new SuccessModal(list)
 		})
 	}
 	if (method === 'POST' && req.path === '/api/blog/add') {
+		let loginCheckResult = loginCheck(req)
+		if (!loginCheckResult) {
+			return loginCheckResult
+		}
 		req.body.createtime = Date.now()
-		req.body.author = 'zhangsan'
+		req.body.author = req.session.username
 		return newBlog(req.body).then((data) => {
 			return new SuccessModal(data)
 		})
 	}
 	if (method === 'POST' && req.path === '/api/blog/update') {
+		let loginCheckResult = loginCheck(req)
+		if (!loginCheckResult) {
+			return loginCheckResult
+		}
 		console.info(req.body, 'req.body')
 		return updateBlog(id, req.body).then((val) => {
 			if (val) {
@@ -39,7 +61,11 @@ const handleBlogRouter = (req, res) => {
 		})
 	}
 	if (method === 'POST' && req.path === '/api/blog/del') {
-		const author = 'zhangsan'
+		let loginCheckResult = loginCheck(req)
+		if (!loginCheckResult) {
+			return loginCheckResult
+		}
+		const author = req.session.username
 		return deleteBlog(id, author).then((val) => {
 			if (val) {
 				return new SuccessModal()
--- src/router/user.js
+++ src/router/user.js
@@ -4,9 +4,9 @@ const { SuccessModal, ErrorModal } = require('../model/resModal')
 
 const handleUserRouter = (req, res) => {
 	const method = req.method
-	if (method === 'GET' && req.path === '/api/user/login') {
-		// const { username, password } = req.body
-		const { username, password } = req.query
+	if (method === 'POST' && req.path === '/api/user/login') {
+		const { username, password } = req.body
+		// const { username, password } = req.query
 		return login(username, password).then((data) => {
 			if (data.username) {
 				// 设置session
@@ -19,16 +19,16 @@ const handleUserRouter = (req, res) => {
 			}
 		})
 	}
-	if (method === 'GET' && req.path === '/api/user/login-test') {
-		if (req.session.username) {
-			return Promise.resolve(
-				new SuccessModal({
-					session: req.session
-				})
-			)
-		} else {
-			return Promise.resolve(new ErrorModal('未登录'))
-		}
-	}
+	// if (method === 'GET' && req.path === '/api/user/login-test') {
+	// 	if (req.session.username) {
+	// 		return Promise.resolve(
+	// 			new SuccessModal({
+	// 				session: req.session
+	// 			})
+	// 		)
+	// 	} else {
+	// 		return Promise.resolve(new ErrorModal('未登录'))
+	// 	}
+	// }
 }
 module.exports = handleUserRouter
